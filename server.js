5c459d28745d969711a1bdf456ed086a4d01925f
unknown <avaneesh.ponna99@gmail.com>
2020-11-04 12:41:32 +0530

    c1

--- server.js
+++ server.js
@@ -13,4 +13,13 @@ res.sendFile(path.join(__dirname+'/dist/Simplyturntask/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err)
+  {
+
+  }
+  else
+  {
+    console.log(`server listening on port number 8080`);
+    }
+});
